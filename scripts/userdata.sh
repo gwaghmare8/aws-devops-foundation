29915db7b492c17ae2b41970bc6ee62085dab0d1
Gaurav Waghmare <gauravwaghmare@Gauravs-MacBook-Air.local>
2026-06-22 17:18:48 +0100

    Fix: add SSM agent for private EC2 Session Manager access

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 sudo yum update -y
+
+# Install nginx
 sudo yum install -y nginx
 systemctl start nginx
 systemctl enable nginx
 echo "Hello from DevOps Foundtation Project" > /usr/share/nginx/html/index.html
+
+# Install SSM Agent
+sudo yum install -y amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
