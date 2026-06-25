5ebb6e62b9179ece156c69888408cbde36d283cf
Gaurav Waghmare <gauravwaghmare@Gauravs-MacBook-Air.local>
2026-06-25 02:39:01 +0100

    resolving conflict

--- environments/phase-1/main.tf
+++ environments/phase-1/main.tf
@@ -31,10 +31,9 @@ module "security_group" {
 }
 
 module "subnet" {
-  source             = "../../modules/subnet"
-  vpc_id             = module.vpc.vpc_id
-  public_cidr        = "10.0.1.0/24"
-  public_subnet_name = "public_subnet"
+    source       = "../../modules/subnet"
+    vpc_id       = module.vpc.vpc_id
+    public_cidr  = "10.0.1.0/24"
 }
 
 module "route_table" {
