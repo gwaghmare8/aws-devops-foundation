41b7c1213bb5b61ace6c4364b1e4b2006ab093b3
gwaghmare8 <gwaghmare8@gmail.com>
2026-06-25 03:21:47 +0100

    feat: phase 2 complete - VPC, NAT, EC2, RDS integration

--- environments/phase-1/main.tf
+++ environments/phase-1/main.tf
@@ -31,9 +31,10 @@ module "security_group" {
 }
 
 module "subnet" {
-    source       = "../../modules/subnet"
-    vpc_id       = module.vpc.vpc_id
-    public_cidr  = "10.0.1.0/24"
+  source             = "../../modules/subnet"
+  vpc_id             = module.vpc.vpc_id
+  public_cidr        = "10.0.1.0/24"
+  public_subnet_name = "public_subnet"
 }
 
 module "route_table" {
